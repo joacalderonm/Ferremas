6a40aa8341dc5e725d372dd311f934a7a8b32ca8
Blate00 <da.coo@duocuc.cl>
2024-04-25 13:39:52 -0400

    estilos

--- FrontEnd/src/layout/Layout.jsx
+++ FrontEnd/src/layout/Layout.jsx
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 
+
 export function Layout ({ children }) {
     return(
         <>
             <Header/>
             <main className='p-4'>
                 {children}
-            </main>
+
+            </main>                
             <Footer/>
         </>
     )
--- FrontEnd/src/pages/Index.jsx
+++ FrontEnd/src/pages/Index.jsx
@@ -1,9 +1,12 @@
 import { Layout } from "../layout/Layout"
-
+import { Ofertas } from '../components/Ofertas'
+import { Slider } from '../components/Slider'
 export function Index(){
     return (
         <Layout>
             {/*Contenido */}
+           <Slider/>
+            <Ofertas/>
         </Layout>
     )
 }
